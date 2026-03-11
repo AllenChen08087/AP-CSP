be1b9f3dbf14bd128098a871eac22dd6a4dc2a31
Allen Chen <chen6home@gmail.com>
2026-03-11 13:28:17 +0800

    first commit

---
+++ Budget.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Budget{
+
+    //procedure: calculate the total spending based on an arraylist
+    public static int calculateTotal(ArrayList<Integer> list) {
+        int sum = 0;
+        for (int i = 0; i< list.size();i++){
+            sum = sum + list.get(i);
+        }
+        return sum;
+    }
+
+    //procedure: check if the spending will exceed the budget, and return true or false based on the budget left and the spending
+    public static boolean exceedBudget (int budget_left, int spending){
+        if (budget_left < spending){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+ 
+    public static void main(String[] args) {
+
+    //variables setup
+    Scanner scanner = new Scanner(System.in);
+    boolean end = false;
+    System.out.println("Enter your budget for the day");
+    int budget = scanner.nextInt();
+    int budget_left = budget;
+    int sum = 0;
+    System.out.println("Nice, now you can input your purchases");
+    ArrayList<Integer> purchases = new ArrayList<Integer>();
+
+    //ask users to input their spendins until either the spendings exceed the budget or the users stop the inputs manually
+    while (end == false){        
+        //ask users to input their purchase, and add it to the arrylist
+        System.out.println("Please input the amount of money you've spent");
+        int purchase = scanner.nextInt();
+
+        //check if the spendings exceed the budget
+        if (exceedBudget(budget_left, purchase)){
+            System.out.println("This spending has exceed your budget");
+        }
+        else{
+            purchases.add(purchase);
+            sum = calculateTotal(purchases);
+            budget_left = budget - sum;
+            System.out.println("You have $" + budget_left + " left");
+            System.out.println("You've spent $" + sum );
+            if (budget_left == 0){
+                end = true;
+            }
+        }
+        //check if the users want to continue inputting purchases
+        if (purchases.size() > 0 && end == false){
+            scanner.nextLine();
+            System.out.println("Would you like to enter another purchase? Answer Yes or No");
+            String response = scanner.nextLine();
+            if (response.equals("No") || response.equals("no")){
+                end = true;
+            } else {
+                end = false;
+            }
+        }
+        
+    }
+    //provide summary to the users on their spendings
+    System.out.println("Thank you for using this App");
+    System.out.println("Your budget for today is $" + budget);
+    System.out.println("You've spent $" + calculateTotal(purchases) + " today");
+    System.out.println("You have $" + budget_left + " left" );
+    
+    scanner.close();
+    }
+}
